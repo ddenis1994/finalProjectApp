b8c67d9346ff53d5cc5ac1fa50c3bc3cd5cf07c1
denis dombrovski <ddenis1994@gmailcom>
2020-04-12 03:01:43 +0300

    add new service class

---
+++ app/src/main/java/com/example/finalprojectapp/data/model/Service.kt
@@ -0,0 +1,30 @@
+package com.example.finalprojectapp.data.model
+
+import androidx.room.Entity
+import androidx.room.Ignore
+import androidx.room.PrimaryKey
+import com.google.firebase.Timestamp
+import com.google.firebase.firestore.Exclude
+import com.google.firebase.firestore.ServerTimestamp
+
+@Entity(tableName = "service")
+data class Service (
+    var name:String,
+    @Ignore
+    @ServerTimestamp
+    val time: Timestamp?,
+    @Ignore
+    val userId: String,
+    @Ignore
+    val credentials: MutableList<Credentials>?,
+    @Exclude
+    @PrimaryKey(autoGenerate = true) val serviceId: Long=0
+){
+    constructor() : this(
+        "",
+        null,
+        "",
+        null
+    )
+
+}
