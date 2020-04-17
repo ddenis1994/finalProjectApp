6ae196e52a51bf3cfdbcca0da368d3ebaaa057c1
denis dombrovski <ddenis1994@gmailcom>
2020-04-17 23:11:59 +0300

    add select what kind of autofill service

--- app/src/main/java/com/example/finalprojectapp/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/example/finalprojectapp/ui/settings/SettingsFragment.kt
@@ -1,12 +1,20 @@
 package com.example.finalprojectapp.ui.settings
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.provider.Settings
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import android.view.autofill.AutofillManager
+import android.widget.CompoundButton
+import android.widget.Switch
+import android.widget.TextView
+import androidx.core.content.ContextCompat.getSystemService
+import androidx.fragment.app.Fragment
 import com.example.finalprojectapp.R
+import kotlinx.android.synthetic.main.fragment_settings.view.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -22,6 +30,8 @@ class SettingsFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    private lateinit var mAutofillManager:AutofillManager
+    private val REQUEST_CODE_SET_DEFAULT = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,14 +39,28 @@ class SettingsFragment : Fragment() {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+        mAutofillManager = getSystemService(this.requireActivity().applicationContext, AutofillManager::class.java)!!
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val root=inflater.inflate(R.layout.fragment_settings, container, false)
+        setupSettingsSwitch(
+            root.settingsSetServiceContainer,
+            R.id.settingsSetServiceLabel,
+            R.id.settingsSetServiceSwitch,
+            mAutofillManager.hasEnabledAutofillServices(),
+            CompoundButton.OnCheckedChangeListener { compoundButton: CompoundButton?, serviceSet: Boolean ->
+                setService(
+                    serviceSet
+                )
+            }
+        )
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+        return root
     }
 
     companion object {
@@ -57,5 +81,44 @@ class SettingsFragment : Fragment() {
                     putString(ARG_PARAM2, param2)
                 }
             }
+    }
+
+    private fun setupSettingsSwitch(
+        containerId: ViewGroup, labelId: Int, switchId: Int, checked: Boolean,
+        checkedChangeListener: CompoundButton.OnCheckedChangeListener) {
+        val container: ViewGroup = containerId
+        val switchLabel =
+            (container.findViewById<View>(labelId) as TextView).text.toString()
+        val switchView =
+            container.findViewById<Switch>(switchId)
+        switchView.contentDescription = switchLabel
+        switchView.isChecked = checked
+        container.setOnClickListener { view: View? -> switchView.performClick() }
+        switchView.setOnCheckedChangeListener(checkedChangeListener)
+    }
+
+    private fun setService(enableService: Boolean) {
+        if (enableService) {
+            startEnableService()
+        } else {
+            disableService()
+        }
+    }
+
+    private fun startEnableService() {
+        if (!mAutofillManager.hasEnabledAutofillServices()) {
+            val intent = Intent(Settings.ACTION_REQUEST_SET_AUTOFILL_SERVICE)
+            intent.data = Uri.parse("package:com.example.android.autofill.service")
+
+            startActivityForResult(
+                intent,
+                REQUEST_CODE_SET_DEFAULT
+            )
+        }
+    }
+
+    private fun disableService() {
+        if (mAutofillManager.hasEnabledAutofillServices())
+            mAutofillManager.disableAutofillServices()
     }
 }
