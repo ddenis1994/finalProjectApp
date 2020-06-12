a7684ab63f92f361cd8bbc49304dc6b2ebf98088
denis dombrovski <ddenis1994@gmailcom>
2020-06-12 23:29:04 +0300

    minnor fixes

--- app/src/main/java/com/example/finalprojectapp/ui/login/register/RegisterPassword.kt
+++ app/src/main/java/com/example/finalprojectapp/ui/login/register/RegisterPassword.kt
@@ -3,12 +3,12 @@ package com.example.finalprojectapp.ui.login.register
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
 import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
 import androidx.navigation.findNavController
@@ -18,36 +18,18 @@ import com.example.finalprojectapp.data.Result
 import com.example.finalprojectapp.data.model.LoggedInUser
 import com.example.finalprojectapp.ui.login.LoginViewModel
 import com.example.finalprojectapp.ui.login.LoginViewModelFactory
-import kotlinx.android.synthetic.main.fragment_login.view.*
 import kotlinx.android.synthetic.main.fragment_register_password.view.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [RegisterPassword.newInstance] factory method to
- * create an instance of this fragment.
- */
+
 class RegisterPassword : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
     private val registerPasswordArgs:RegisterPasswordArgs by navArgs()
     private val registerPasswordViewModel:RegisterPasswordViewModel by activityViewModels()
     private  val loginViewModel: LoginViewModel by activityViewModels{
         LoginViewModelFactory(requireActivity())
     }
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -144,23 +126,5 @@ class RegisterPassword : Fragment() {
     }
 
 
-        companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment RegisterPassword.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            RegisterPassword().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+
 }
